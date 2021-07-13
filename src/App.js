8ef64d1f494e1d03547e26a81b4673bf0173db82
Asguerrero07 <you@example.com>
2021-07-12 19:33:36 -0500

    Added withRouter to menu-items to facilitate navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx'
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+      <Switch>
+        <Route exact path='/' component={ HomePage} />
+        <Route path='/hats' component={HomePage} />
+      </Switch>
+      
     </div>
   );
 }
