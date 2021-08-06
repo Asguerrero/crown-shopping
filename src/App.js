8a1140a4de0b2bdbe0fe1a940f0de0ca4c2c957a
Asguerrero07 <you@example.com>
2021-08-05 21:13:50 -0500

    Added cartIcon and cartDrodown components and implemented cart reducer

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import HomePage from './pages/homepage/homepage.component.jsx'
 import ShopPage from './pages/shop/shop.component.jsx'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sigup.component.jsx'
 import Header from './components/header/header.component.jsx'
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { auth, createUserProfileDocument} from './firebase/firebase.utils';
 import { connect } from 'react-redux';
 import { setCurrentUser } from './redux/user/user.actions'
@@ -47,7 +47,9 @@ class App extends React.Component {
        <Switch>
         <Route exact path='/' component={ HomePage} />
         <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInAndSignUpPage} />
+        <Route exact path='/signin' 
+        render ={ () => this.props.currentUser ? <Redirect to='/'/> : <SignInAndSignUpPage/> }
+        />
       </Switch> 
       
     </div>
@@ -55,9 +57,13 @@ class App extends React.Component {
 }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
